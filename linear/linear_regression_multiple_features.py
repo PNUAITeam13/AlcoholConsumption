4032b3a28575d879f3046b741907be4be24f25b7
Anastasiia Tsapko <anastsapko@gmail.com>
2023-11-20 20:06:52 +0200

    [STARTAI-15] Enhanced Privacy and Initialization Handling for Model Parameters

--- linear/linear_regression_multiple_features.py
+++ linear/linear_regression_multiple_features.py
@@ -1,21 +1,16 @@
 import numpy as np
 
-
 class LinearRegressionMultipleFeatures:
-    
     def __init__(self, n_estimators, learning_rate):
         self.__n_estimators = n_estimators
         self.__learning_rate = learning_rate
-        self.m = None
-        self.b = None
-        self.loss_history = []
 
     def gradient_descent(self, X, y):
         predictions = self.predict(X)
         errors = predictions - y.reshape(-1)
 
-        self.m -= self.__learning_rate * (1/X.shape[0]) * X.T.dot(errors)
-        self.b -= self.__learning_rate * (1/X.shape[0]) * np.sum(errors)
+        self.__m -= self.__learning_rate * (1 / X.shape[0]) * X.T.dot(errors)
+        self.__b -= self.__learning_rate * (1 / X.shape[0]) * np.sum(errors)
 
     def loss_function(self, X, y):
         predictions = self.predict(X)
@@ -27,14 +22,15 @@ class LinearRegressionMultipleFeatures:
         X = X.values
         y = y.values.reshape(-1, 1)
 
-        self.m = np.zeros(X.shape[1])
-        self.b = 0
+        self.__m = np.zeros(X.shape[1])
+        self.__b = 0
+        self.__loss_history = []
 
         for i in range(self.__n_estimators):
             self.gradient_descent(X, y)
 
             current_loss = self.loss_function(X, y)
-            self.loss_history.append(current_loss)
+            self.__loss_history.append(current_loss)
 
     def predict(self, X):
-        return (np.dot(X, self.m) + self.b).flatten()
+        return (np.dot(X, self.__m) + self.__b).flatten()
